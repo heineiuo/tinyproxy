23c20c0c54ce5bfaeeabbc494e535ddb1a612e68
heineiuo <heineiuo@gmail.com>
2018-02-14 18:56:32 +0800

    update lanproxy example

--- example/client.js
+++ example/client.js
@@ -7,12 +7,17 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(compression())
+app.use(express.json())
 app.get('/', (req, res) => {
   res.end('hello world')
 })
 
 app.get('/image', (req, res) => {
   res.sendFile(__dirname + '/image.png')
+})
+
+app.post('/json', (req, res) => {
+  res.json(req.body)
 })
 
 app.listen(10835, () => {
@@ -31,27 +36,38 @@ const client = new SeashellClient()
 const target = 'http://localhost:10835'
 
 client.createServer((req, res) => {
-  console.log(req.headers.url)
+  // delete req.headers.httpHeaders.host
+  // delete req.headers.httpHeaders.connection
+  console.log(req.headers)
   const targetReq = http.request({
+    protocol: 'http:',
     hostname: 'localhost',
     port: 10835,
     path: req.headers.url,
-    method: req.headers.method || 'GET',
-    headers: {
-    }
+    method: req.headers.httpMethod || 'GET',
+    headers: req.headers.httpHeaders || {},
   }, (targetRes) => {
     targetRes.on('data', (chunk) => {
       res.write(chunk)
-    });
+    })
     targetRes.on('end', () => {
       res.end()
     })
   })
 
-  targetReq.end()
+  req.on('data', (data) => {
+    console.log(data)
+    if (!data) return
+    targetReq.write(data)
+  })
+
+  req.on('end', () => {
+    console.log('end')
+    targetReq.end()
+  })
 
 }, {
-  serverAddress: 'ws://localhost:8080?id=lanproxy.localhost&secret=bbb3919786197a50622d14f2424fc12f540710dae88105a59c61613e189a'
-})
+    serverAddress: 'ws://localhost:8080?id=lanproxy.localhost&secret=bbb3919786197a50622d14f2424fc12f540710dae88105a59c61613e189a'
+  })
 
 console.log('client started')
